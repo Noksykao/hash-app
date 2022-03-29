cead5ca6df582165c2b358765aee888c02c8dc1f
Andrey <Andrey.aleshkin.93@mail.ru>
2022-03-29 20:10:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        Person p1 = new Person();
+        p1.setId(555);
+        p1.setName("Bob");
+        Person p2 = new Person();
+        p2.setId(100);
+        p2.setName("John");
+
+        System.out.println(p1.equals(p2));
+
+        Person p3 = p2;
+        System.out.println(p2.equals(p3));
+    }
+}
+
+class Person{
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+
+    }
+
